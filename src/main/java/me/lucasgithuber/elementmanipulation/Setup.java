1d87a8b2c734a87734f55b45602e1427fa56b8a9
lucasGithuber <lulukas.2022@gmail.com>
2022-01-21 14:57:59 -0300

    woopsie

--- src/main/java/me/lucasgithuber/elementmanipulation/Setup.java
+++ src/main/java/me/lucasgithuber/elementmanipulation/Setup.java
@@ -14,6 +14,6 @@ public class Setup {
 
         new EMDeconstructor(Categories.EMMachines).register(p);
         new EMUltraCompressor(Categories.EMMachines).register(p);
-        new EMJunctionTable(Categories.EMMachines).register(p);
+
     }
 }
